5f4e8c2dc8e38e5b4d10bbc75a16fbe3af7b8d78
Marifel <mrbarbasa@gmail.com>
2015-01-19 20:47:40 -1000

    Finish Level 1

--- build_arrays.js
+++ build_arrays.js
@@ -3,54 +3,55 @@
  **/
 
 // Create an empty array.
-
+var empty = [];
 
 // Create an array of the colors of the rainbow.
-
+var rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'violet', 'indigo'];
 
 // Create an array of gems.
-
+var gems = ['ruby', 'emerald', 'sapphire'];
 
 // Create an array of military ranks.
-
+var ranks = ['ensign', 'lieutenant', 'commander', 'captain', 'admiral'];
 
 // Create an array of road signs.
-
+var signs = ['stop', 'yield', 'no right-turn', 'no U-turn'];
 
 // Create an array containing components of a tree.
-
+var tree = ['trunk', 'roots', 'branches', 'leaves'];
 
 // Create an array of prime numbers less than 20.
-
+var primes = [2, 3, 5, 7, 11, 13, 17, 19];
 
 // Create an array of words programmers typically say while programming.
-
+var devtalk = ['asynchronous', 'performance', 'callback', 'environment', 'production'];
 
 // Create an array of elements of the periodic table.
-
+var elements = ['carbon', 'sodium', 'lithium', 'potassium'];
 
 // Create an array representing the coins currently in your pocket.
-
+var coins = ['quarter', 'nickel'];
 
 // Create an array of items you bring to class daily.
-
+var stuff = ['lunch', 'water', 'bag', 'backpack'];
 
 // Create an array of web developer role titles.
-
+var roles = ['UX designer', 'UI developer', 'JavaScript developer'];
 
 // Create an array of good luck charms.
-
+var charms = ['four-leaf clover', 'buttercup'];
 
 // Create an array of mythical animals.
-
+var animals = ['unicorns', 'pentacorns'];
 
 // Create an array of drinks.
-
+var drinks = ['juice', 'milk', 'tea', 'coffee'];
 
 // Create an array of emoticons.
-
+var emoticons = ['happy', 'sad', 'angry', 'calm'];
 
 // Create an array with the letters of the word 'array'.
+var letters = ['a', 'r', 'r', 'a', 'y'];
 
 
 
